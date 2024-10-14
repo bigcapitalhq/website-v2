0871c587db307e7b05010bb1c60e062f9ac8baac
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2024-10-14 14:41:04 +0200

    feat: Add back link to single update page

--- src/app/updates/[id]/page.tsx
+++ src/app/updates/[id]/page.tsx
@@ -1,6 +1,7 @@
 import clsx from 'classnames';
 import { getUpdateData } from '@/lib/updates';
 import styles from '../page.module.css';
+import Link from 'next/link';
 
 interface PageProps {
   params: {
@@ -24,8 +25,17 @@ export default async function UpdatesPage({ params }: PageProps) {
   return (
     <div>
       <article className="flex items-start md:flex-row flex-col gap-6 mb-16 pb-10 overflow-wrap border-b-[1px] border-[#E5E8EB] last:border-b-[0px]">
-        <div className="md:sticky md:w-1/4 text-sm md:mt-2.5">
+        <div className="md:sticky md:w-1/4 text-sm md:mt-2.5 flex flex-col">
           <span className={'text-[#5f6b7c]'}>{update.formattedDate}</span>
+
+          <p className={'mt-4'}>
+            <Link
+              href={'/updates'}
+              className={'text-[#404040] hover:underline hover:text-indigo'}
+            >
+              ← Back to Updates
+            </Link>
+          </p>
         </div>
 
         <div className="max-w-[588px]">
